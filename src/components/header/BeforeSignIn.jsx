7d7ed81f65490795caafff7f8b887f1ba812f437
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-06-25 22:48:22 +0600

    login button design change

--- src/components/header/BeforeSignIn.jsx
+++ src/components/header/BeforeSignIn.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const BeforeSignIn = () => {
     return (
         <div className="flex flex-row gap-2 ml-5 ">
-           <Link to='/login' className="bg-[black] py-2 px-3 rounded-full text-white font-normal hover:shadow-lg hover:scale-[1.05] ease-in-out duration-150">SIGN IN</Link>
+           <Link to='/login' className="bg-[black] py-2 px-3 rounded-full text-white font-normal hover:shadow-lg hover:scale-[1.05] ease-in-out duration-150 whitespace-nowrap">SIGN IN</Link>
         </div>
     )
 }
